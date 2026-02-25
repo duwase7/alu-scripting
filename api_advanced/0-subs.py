ef62d4d4148f5facf1db203968906c46383bcb7c
duwase7 <d.uwase7@alustudent.com>
2026-02-25 23:51:42 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -20,23 +20,4 @@ def number_of_subscribers(subreddit):
         return data['data']['subscribers']
 
     else:
-        return 0#!/usr/bin/python3
-"""DOC"""
-import requests
-
-
-def number_of_subscribers(subreddit):
-    """DOC"""
-    reddit_url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-
-    header = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url,
-                            headers=header
-                            )
-
-    if response.status_code == 200:
-        data = response.json()['data']
-        subs = data['subscribers']
-        return subs
-    return 0
+        return 0
