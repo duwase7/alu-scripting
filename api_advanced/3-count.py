0607036e15c1208381805f0f6213ff7b5746ce7a
duwase7 <d.uwase7@alustudent.com>
2026-02-25 22:11:11 +0200

    Update 3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,55 +1,50 @@
 
-#!/usr/bin/python3
-""" 3-count.py """
-import json
+"""Count keywords in hot posts recursively"""
 import requests
+import re
 
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """ prints a sorted count of given keywords """
+def count_words(subreddit, word_list, after=None, counts=None):
+    """Print sorted keyword counts"""
+    if counts is None:
+        counts = {}
 
-    if after == "":
-        count = [0] * len(word_list)
+        for word in word_list:
+            word = word.lower()
+            counts[word] = counts.get(word, 0)
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'User-Agent': 'Mozilla/5.0'})
+    headers = {'User-Agent': 'alu-api-advanced/1.0'}
+    params = {'after': after}
 
-    if request.status_code == 200:
-        data = request.json()
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
 
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
+    if response.status_code != 200:
+        return
 
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
+    data = response.json().get('data')
 
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
+    for post in data.get('children'):
+        title = post.get('data').get('title').lower()
+        words = re.findall(r'\b\w+\b', title)
 
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-            count_words(subreddit, word_list, after, count)
+        for word in words:
+            if word in counts:
+                counts[word] += 1
 
+    after = data.get('after')
+
+    if after is not None:
+        return count_words(subreddit, word_list, after, counts)
+
+    sorted_words = sorted(
+        counts.items(),
+        key=lambda x: (-x[1], x[0])
+    )
+
+    for word, count in sorted_words:
+        if count > 0:
+            print("{}: {}".format(word, count))
