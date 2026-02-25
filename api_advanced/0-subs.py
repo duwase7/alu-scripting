3bdfe0e7503d3322bcbe44dd7a78404fab90d617
duwase7 <d.uwase7@alustudent.com>
2026-02-25 22:17:41 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,9 +8,11 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'alu-api-advanced/1.0'}
 
-    response = requests.get(url,
-                            headers=headers,
-                            allow_redirects=False)
+    response = requests.get(
+    reddit_url,
+    headers=header,
+    allow_redirects=False
+)
 
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
