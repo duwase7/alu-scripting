070b54146821af8309ea70ed405502730d73c04e
duwase7 <d.uwase7@alustudent.com>
2026-02-25 22:10:28 +0200

    Update 2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,28 +1,32 @@
 
-#!/usr/bin/python3
-"""docs"""
+"""Recursive function to get all hot post titles"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """"Doc"""
-    url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    resopnse = requests.get(url, headers=header, params=param)
+def recurse(subreddit, hot_list=None, after=None):
+    """Return list of all hot post titles"""
+    if hot_list is None:
+        hot_list = []
 
-    if resopnse.status_code != 200:
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'alu-api-advanced/1.0'}
+    params = {'after': after}
+
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
         return None
-    else:
-        json_res = resopnse.json()
-        after = json_res.get('data').get('after')
-        has_next = \
-            json_res.get('data').get('after') is not None
-        hot_articles = json_res.get('data').get('children')
-        [hot_list.append(article.get('data').get('title'))
-         for article in hot_articles]
 
-        return recurse(subreddit, hot_list, after=after) \
-            if has_next else hot_list
+    data = response.json().get('data')
+    after = data.get('after')
 
+    for post in data.get('children'):
+        hot_list.append(post.get('data').get('title'))
+
+    if after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, after)
