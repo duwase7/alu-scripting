aae07f148bf966388d8430234ae79adff2b99a33
duwase7 <d.uwase7@alustudent.com>
2026-03-01 14:30:45 +0200

    Refactor number_of_subscribers function for clarity

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,23 +1,22 @@
+cat > api_advanced/0-subs.py << 'EOF'
 #!/usr/bin/python3
 """
 A script that use reddit api to outputs the
 number of subscribers of a certain subreddit.
 """
 
-
-import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    """Returns the number of subscribers for a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = json.loads(response.text)
+        data = response.json()
         return data['data']['subscribers']
-
     else:
         return 0
+EOF
