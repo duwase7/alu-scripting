f5aca7b38cd49a1cf90355d53a93075b7b7f9fc6
duwase7 <d.uwase7@alustudent.com>
2026-03-01 14:55:20 +0200

    Implement number_of_subscribers function
    Added a function to retrieve the number of subscribers from a subreddit using the Reddit API.

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -1,12 +1,19 @@
+cat > api_advanced/0-subs.py << 'EOF'
 #!/usr/bin/python3
-"""
-0-main
-"""
-import sys
+"""Returns number of subscribers for a given subreddit."""
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        return 0
+EOF
